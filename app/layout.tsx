648ea12595a48384cc3e0fdbcff8150d21bb3c17
Nadal <nadalibriz@gmail.com>
2026-08-27 18:02:20 +0200

    V1.11

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import XpTracker from "@/components/XpTracker";
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
             <body className="min-h-full flex flex-col bg-slate-50/30 text-slate-900">
                 <XpTracker />
                 {children}
+                <Analytics /> 
             </body>
         </html>
     );
